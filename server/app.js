2c163672fe822fd8795f4ca5522850f6edde5436
Fedor_Semochkin <221800ss@gmail.com>
2024-01-24 16:08:14 +0700

    update port

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const { v4: uuidv4 } = require('uuid');
 
 
 const server = express();
-const port = process.env.PORT || 3000;
+const port =  3000;
 
 server.use(pinoMiddleware);
 server.use(cookieParser())
